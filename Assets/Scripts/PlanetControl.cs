2ed826b623d9e02fbbbf381d2b3763a02982f815
Tarjei Husøy <git@thusoy.com>
2017-03-07 10:03:56 -0800

    Add MVP
    Sort-of working with three suns and a planet, want to scale up stuff to
    realistic sizes and make the planet super dense but walkable, so that
    a character can walk on the surface and observe the suns' movement.
    
    Bonus is enablin fast-forwarding time, adding a starry sky backdrop to
    see relative movement, making the suns light sources, estimating surface
    temperatures on the planet, etc.

---
+++ Assets/Scripts/PlanetControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlanetControl : MonoBehaviour {
+	private List<Rigidbody> celestialBodies;
+	// gravitational constant
+	private double G = 1.6 * Mathf.Pow(10, 0);
+
+	void Start () {
+		// Set initial velocities
+		Rigidbody sola = GameObject.Find ("solb").GetComponent<Rigidbody> ();
+		Rigidbody solb = GameObject.Find ("sola").GetComponent<Rigidbody> ();
+		Rigidbody solc = GameObject.Find ("solc").GetComponent<Rigidbody> ();
+		Rigidbody planet = GameObject.Find ("planet").GetComponent<Rigidbody> ();
+		sola.AddForce (3000.0f, 5066.0f, -2005.0f);
+		solb.AddForce (-3000.0f, -9000.0f, 1000.0f);
+		solc.AddForce (20.0f, 00.0f, -7000.0f);
+		planet.AddForce (20.0f, 00.0f, 20.0f);
+		GameObject.Find ("planetcam").GetComponent<Camera> ().transform.parent = planet.transform;
+
+		celestialBodies = new List<Rigidbody> ();
+		celestialBodies.Add (sola);
+		celestialBodies.Add (solb);
+		celestialBodies.Add (solc);
+		celestialBodies.Add (planet);
+	}
+
+	// Use this for initialization
+	void FixedUpdate () {
+		foreach (Rigidbody body in celestialBodies) {
+			foreach (Rigidbody otherBody in celestialBodies) {
+				if (otherBody == body) {
+					continue;
+				}
+				Vector3 distance = otherBody.position - body.position;
+				Vector3 force = (float) G * otherBody.mass * body.mass * distance / distance.sqrMagnitude;
+
+				body.AddForce(force);
+			}
+		}
+	}
+}
